e13755d6faeb73d966228982688da3a7a3cebdc4
markogarza <markogarza@gmail.com>
2023-05-28 16:18:32 -0400

    Got elements by ID

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const buttonTwitter = document.getElementById("twitter");
+const buttonNewQuote = document.getElementById("new-quote");
+
 const url = "https://type.fit/api/quotes";
 let data = [];
 
 const newQuote = () => {
   const quote = data[Math.floor(Math.random() * data.length)];
-  console.log(quote);
+  quoteText.textContent = quote.text;
+  quoteAuthor.textContent = quote.author;
 };
 
 const getQuotes = async () => {
